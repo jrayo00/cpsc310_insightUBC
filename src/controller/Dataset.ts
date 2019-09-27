ad7c832a8e7d51e23db3dc0de82fbeb4093c7f90
Joshua Rayo <j.rayo123@gmail.com>
2019-09-27 00:04:05 -0700

    fix promise handling error. add dataset works otherwise

--- src/controller/Dataset.ts
+++ src/controller/Dataset.ts
@@ -12,19 +12,19 @@ export class Dataset {
     public parseData(data: string) {
         let obj = JSON.parse(data);
         Log.test(obj.result.length);
-        for (let i = 0; i < obj.result.length; i++) {
+        for (let item of obj.result) {
             // iterate through result array and get the key value pairs
             let newSection: Section = new Section();
-            newSection.info.Subject = obj.result[i].Subject;
-            newSection.info.Course = obj.result[i].Course;
-            newSection.info.Avg = obj.result[i].Avg;
-            newSection.info.Professor = obj.result[i].Professor;
-            newSection.info.Title = obj.result[i].Title;
-            newSection.info.Pass = obj.result[i].Pass;
-            newSection.info.Fail = obj.result[i].Fail;
-            newSection.info.Audit = obj.result[i].Audit;
-            newSection.info.id = obj.result[i].id;
-            newSection.info.Year = obj.result[i].Year;
+            newSection.info.Subject = item.Subject;
+            newSection.info.Course = item.Course;
+            newSection.info.Avg = item.Avg;
+            newSection.info.Professor = item.Professor;
+            newSection.info.Title = item.Title;
+            newSection.info.Pass = item.Pass;
+            newSection.info.Fail = item.Fail;
+            newSection.info.Audit = item.Audit;
+            newSection.info.id = item.id;
+            newSection.info.Year = item.Year;
             this.allSections.push(newSection);
         }
         Log.test("STOP");
