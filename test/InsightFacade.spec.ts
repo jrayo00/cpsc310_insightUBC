10e81196e0bb1a7ab3a48f851b9dea9016cd9783
Joshua Rayo <j.rayo123@gmail.com>
2019-10-02 13:11:44 -0700

    test autobot timeo

--- test/InsightFacade.spec.ts
+++ test/InsightFacade.spec.ts
@@ -202,7 +202,7 @@ describe("InsightFacade Add/Remove Dataset", function () {
 /*
  * This test suite dynamically generates tests from the JSON files in test/queries.
  * You should not need to modify it; instead, add additional files to the queries directory.
- * You can still make tests the normal way, this is just a convenient tool for a majority of queries.
+ * You can still make tests the normal way, this is just a convenient tool for a majority of queries. hgg
  */
 describe("InsightFacade PerformQuery", () => {
     const datasetsToQuery: { [id: string]: any } = {
