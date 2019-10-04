1d282dd748372800cd050d283805ff343b4abf1f
eugenie lai <eugenie.lai@alumni.ubc.ca>
2019-10-04 11:21:40 -0700

    attempt to fix the null query case

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -151,9 +151,7 @@ export default class InsightFacade implements IInsightFacade {
             if (result.length > 5000) {
                 return Promise.reject(new ResultTooLargeError());
             } else {
-                if (result === undefined || result === null) {
-                    return Promise.reject(new InsightError("Result is null or undefined"));
-                } else {return Promise.resolve(result); }
+                return Promise.resolve(result);
             }
         }).catch((err: any) => {
             Log.info(`In performQuery ${err}`);
