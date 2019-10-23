ecef8e36b12a91a43a36699d0cebe1459888e76d
Joshua Rayo <j.rayo123@gmail.com>
2019-10-23 01:11:58 -0700

    fix extracting data from tags

--- src/controller/Dataset.ts
+++ src/controller/Dataset.ts
@@ -9,6 +9,9 @@ export class Dataset {
     public id: string;
     public numRows: number = 0;
     public kind: InsightDatasetKind;
+    private bodyElement  = {};
+    private htmlElement = {};
+    private tableElement = {};
     constructor(id: string, datasetType: InsightDatasetKind) {
         Log.trace("Dataset::init()");
         this.id = id;
@@ -55,47 +58,76 @@ export class Dataset {
     public parseRoomsDataset(data: string) {
         const parse5 = require("parse5");
         const document = parse5.parse(data);
-        let tableElement: JSON;
-        tableElement = this.findTableElement(document);
-        Log.test(JSON.stringify(tableElement));
+        this.findTableElement(document);
+        // Log.test(JSON.stringify(this.tableElement));
+        this.parseTableElement(this.tableElement);
     }
 
-    private findTableElement(document: any): JSON {
+    private findTableElement(document: any) {
         // TODO: find html element before finding body
-        let bodyElement: JSON;
-        bodyElement = this.findBodyElementRecursive(document);
-        return this.findTableElementRecursive(bodyElement);
+        this.findHTMLTagRecursive(document, "html");
+        this.findHTMLTagRecursive(this.htmlElement, "body");
+        this.findHTMLTagRecursive(this.bodyElement, "table");
     }
 
-    private findTableElementRecursive(node: any): JSON {
-        for (let k in node) {
-            let key: string = k;
-            if (key === "nodeName") {
-                let tmp = node[key];
-                if (node[key] === "table") {
-                    return node;
-               }
-            } else if (key === "childNodes") {
-                for (let element in node[key]) {
-                    return this.findTableElementRecursive(node[key][element]);
-                }
+    private findHTMLTagRecursive(node: any, tagName: string) {
+        // for (let k in node) {
+        //     let key: string = k;
+        //     if (key === "nodeName") {
+        //         let tmp = node[key];
+        //         if (node[key] === tagName) {
+        //             return node;
+        //         }
+        //     } else if (key === "childNodes") {
+        //         for (let element of node[key]) {
+        //             let cNode = node[key];
+        //             let obj = node[key].element;
+        //             Log.test(obj);
+        //             return this.findHTMLTagRecursive(element, tagName);
+        //         }
+        //     }
+        // }
+        if (node.nodeName === tagName) {
+            switch (tagName) {
+                case "html":
+                    this.htmlElement = node;
+                    break;
+                case "body":
+                    this.bodyElement = node;
+                    break;
+                case "table":
+                    this.tableElement = node;
+                    break;
+            }
+            return;
+        }
+        if (node.childNodes !== undefined) {
+            for (let obj of node.childNodes) {
+                this.findHTMLTagRecursive(obj, tagName);
             }
         }
     }
 
-    private findBodyElementRecursive(node: any): JSON {
-        for (let k in node) {
-            let key: string = k;
-            if (key === "nodeName") {
-                let tmp = node[key];
-                if (node[key] === "body") {
-                    return node;
-                }
-            } else if (key === "childNodes") {
-                for (let element in node[key]) {
-                    return this.findBodyElementRecursive(node[key][element]);
-                }
+    private parseTableElement(node: any) {
+        let tbody: any;
+        let count: number = 0;
+        for (let obj of node.childNodes) {
+            if (obj.nodeName === "tbody") {
+                tbody = obj;
+                break;
             }
         }
+        for (let obj of tbody.childNodes) {
+            if (obj.nodeName === "tr") {
+                this.extractRoomInfo(obj);
+                count++;
+            }
+        }
+        Log.test(count + "");
+    }
+
+    private extractRoomInfo(obj: ChildNode) {
+        let newRoom: Room = new Room();
+
     }
 }
