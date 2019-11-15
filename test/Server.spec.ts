57742056a240e24ec3a82613f1b311ea8ed23958
eugenie lai <eugenie.lai@alumni.ubc.ca>
2019-11-15 10:50:09 -0800

    push eugy_d3

--- test/Server.spec.ts
+++ test/Server.spec.ts
@@ -27,6 +27,7 @@ describe("Facade D3", function () {
     // Added fields
     const cacheDir = __dirname + "/../data";
     const serverURL = "http://localhost:4321";
+    const testQueries: ITestQuery[] = TestUtil.readTestQueries("test/serverQueries");
 
     chai.use(chaiHttp);
 
@@ -49,31 +50,24 @@ describe("Facade D3", function () {
         }).catch(function (err: Error) {
             Log.error("Server.spec.ts::after() - ERROR: " + err.message);
         });
+        try {
+            fs.removeSync(cacheDir);
+            fs.mkdirSync(cacheDir);
+            facade = new InsightFacade();
+        } catch (err) {
+            Log.error(err);
+        }
         Log.test(`After: ${this.test.parent.title}`);
     });
 
     beforeEach(function () {
         // might want to add some process logging here to keep track of what"s going on
         Log.test(`BeforeTest: ${this.currentTest.title}`);
-        try {
-            fs.removeSync(cacheDir);
-            fs.mkdirSync(cacheDir);
-            facade = new InsightFacade();
-        } catch (err) {
-            Log.error(err);
-        }
     });
 
     afterEach(function () {
         // might want to add some process logging here to keep track of what"s going on
         Log.test(`AfterTest: ${this.currentTest.title}`);
-        try {
-            fs.removeSync(cacheDir);
-            fs.mkdirSync(cacheDir);
-            facade = new InsightFacade();
-        } catch (err) {
-            Log.error(err);
-        }
     });
 
     // Sample on how to format PUT requests
@@ -93,6 +87,64 @@ describe("Facade D3", function () {
                 })
                 .catch(function (err) {
                     // some logging here please!
+                    expect.fail();
+                });
+        } catch (err) {
+            // and some more logging here!
+        }
+    });
+
+    it("POST tests for datasets", function () {
+        const endpointURL = "/query";
+        for (const test of testQueries) {
+            it(`[${test.filename}] ${test.title}`, function (done) {
+                try {
+                    return chai.request(serverURL)
+                        .put(endpointURL)
+                        .send(test.query)
+                        .set("Content-Type", "application/json")
+                        .then(function (res: Response) {
+                            TestUtil.checkQueryResult(test, res.body["result"], done);
+                        })
+                        .catch(function (err) {
+                            TestUtil.checkQueryResult(test, err, done);
+                        });
+                } catch (err) {
+                    // and some more logging here!
+                }
+            });
+        }
+    });
+
+    it("Failed DELETE test for courses dataset for InsightError", function () {
+        const endpointURL = "/dataset/courses_";
+        const expected = Error;
+        try {
+            return chai.request(serverURL)
+                .del(endpointURL)
+                .set("Content-Type", "application/json")
+                .then(function (res: Response) {
+                    expect.fail();
+                })
+                .catch(function (err) {
+                    expect(err).to.be.instanceOf(expected);
+                });
+        } catch (err) {
+            // and some more logging here!
+        }
+    });
+
+    it("Success DELETE test for courses dataset", function () {
+        const endpointURL = "/dataset/courses0";
+        try {
+            return chai.request(serverURL)
+                .del(endpointURL)
+                .set("Content-Type", "application/json")
+                .then(function (res: Response) {
+                    expect(res.status).to.be.equal(200);
+                })
+                .catch(function (err) {
+                    Log.info(err);
                     expect.fail();
                 });
         } catch (err) {
@@ -160,7 +212,7 @@ describe("Facade D3", function () {
         }
     });
 
-    it("Failed DELETE test for courses dataset", function () {
+    it("Failed DELETE test for courses dataset for NoFound Error", function () {
         const endpointURL = "/dataset/courses";
         const expected = Error;
         try {
@@ -194,78 +246,3 @@ describe("Facade D3", function () {
         }
     });
 });
-
-describe("Facade D3 DELETE, POST, GET", function () {
-    let facade: InsightFacade = null;
-    let server: Server = null;
-    // Added fields
-    const cacheDir = __dirname + "/../data";
-    const serverURL = "http://localhost:4321";
-    // Read queries
-    const testQueries: ITestQuery[] = TestUtil.readTestQueries("test/serverQueries");
-
-    chai.use(chaiHttp);
-    before(function () {
-        Log.test(`Before all`);
-        facade = new InsightFacade();
-        server = new Server(4321);
-        server.start().then(function (val: boolean) {
-            Log.info("Server.spec.ts::before() - started: " + val);
-        }).catch(function (err: Error) {
-            Log.error("Server.spec.ts::before() - ERROR: " + err.message);
-        });
-    });
-
-    after(function () {
-        server.stop().then(function (val: boolean) {
-            Log.info("Server.spec.ts::after() - stopped: " + val);
-        }).catch(function (err: Error) {
-            Log.error("Server.spec.ts::after() - ERROR: " + err.message);
-        });
-        try {
-            fs.removeSync(cacheDir);
-            fs.mkdirSync(cacheDir);
-            facade = new InsightFacade();
-        } catch (err) {
-            Log.error(err);
-        }
-        Log.test(`After: ${this.test.parent.title}`);
-    });
-
-    beforeEach(function () {
-        // might want to add some process logging here to keep track of what"s going on
-        return facade.addDataset("courses", buffers["courses"].toString("base64"), InsightDatasetKind.Courses)
-            .then((r) => {
-                Log.info("Server.spec.ts::before() - loaded courses dataset: " + r);
-                Log.test(`BeforeTest: ${this.currentTest.title}`);
-            });
-    });
-
-    afterEach(function () {
-        // might want to add some process logging here to keep track of what"s going on
-        Log.test(`AfterTest: ${this.currentTest.title}`);
-    });
-
-    it("POST tests for datasets", function () {
-        const endpointURL = "/query";
-        for (const test of testQueries) {
-            it(`[${test.filename}] ${test.title}`, function (done) {
-                try {
-                    return chai.request(serverURL)
-                        .put(endpointURL)
-                        .send(test.query)
-                        .set("Content-Type", "application/x-zip-compressed")
-                        .then(function (res: Response) {
-                            TestUtil.checkQueryResult(test, res.body["result"], done);
-                        })
-                        .catch(function (err) {
-                            TestUtil.checkQueryResult(test, err, done);
-                        });
-                } catch (err) {
-                    // and some more logging here!
-                }
-            });
-        }
-    });
-});
-
