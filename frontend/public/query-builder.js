f784c75c40043d2cbf915072e3c710283bb75e46
Joshua Rayo <j.rayo123@gmail.com>
2019-12-05 00:04:17 -0800

    test commit

--- frontend/public/query-builder.js
+++ frontend/public/query-builder.js
@@ -2,7 +2,7 @@
 /**
  * Builds a query object using the current document object model (DOM).
  * Must use the browser's global document object {@link https://developer.mozilla.org/en-US/docs/Web/API/Document}
- * to read DOM information.
+ * to read DOM information1.
  *
  * @returns query object adhering to the query EBNF
  */
